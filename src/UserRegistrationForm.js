979fa24e0e2eec94eaa628ee9df96e27ed6d6b88
Thomas Duxbury <thomas.duxbury90@gmail.com>
2022-12-01 22:44:59 +0000

    Added functionality to post form details to user API

--- src/UserRegistrationForm.js
+++ src/UserRegistrationForm.js
@@ -1,18 +1,85 @@
 import { useState } from "react"
 
-// const saveUser = (user) => {
-//   fetch("http://127.0.0.1:8080/users", {
-//     method: "POST",
-//     body: user,
-//     mode: "no-cors"
-//   }).then((response) => response.json()).then((data) => console.log(data));
-// }
-
 export const UserRegistrationForm = () => {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [postcode, setPostcode] = useState("");
+
+  const [passwordInvalid, setPasswordInvalid] = useState(false);
+  const [emailInvalid, setEmailInvalid] = useState(false);
+  const [postcodeInvalid, setPostcodeInvalid] = useState(false);
+
+  const [userCreated, setUserCreated] = useState(false);
+  const [duplicateEmail, setDuplicateEmail] = useState(false);
+
+  const createUser = () => {
+    const user = {
+      name: name,
+      email: email,
+      password: password,
+      postcode: postcode.toUpperCase()
+    }
+    return JSON.stringify(user);
+  }
+
+  const saveUser = () => {
+    setDuplicateEmail(false)
+    setUserCreated(false)
+    if (inputValid()) {
+      const res = fetch("http://127.0.0.1:8080/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": 'application/json'
+        },
+        body: createUser(),
+      })
+      res.then((res) => {
+        if (res.status == 200) {
+          setUserCreated(true);
+          setName("")
+          setEmail("")
+          setPassword("")
+          setPostcode("")
+        } else if (res.status == 500) {
+          setDuplicateEmail(true);
+        }
+      })
+    }
+  }
+
+
+  const inputValid = () => {
+    setPasswordInvalid(false);
+    setEmailInvalid(false);
+    setPostcodeInvalid(false);
+    let errors = false;
+    if (password.length === 0) {
+      setPasswordInvalid(true);
+      errors = true;
+    }
+    if (postcode.length > 0 && !validPostcode(postcode)) {
+      setPostcodeInvalid(true);
+      errors = true;
+    }
+    if (email.length == 0 || !validEmail(email)) {
+      setEmailInvalid(true)
+      errors = true;
+    }
+    return !errors;
+  }
+
+  const validEmail = (email) => {
+    const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    return regex.test(email);
+  }
+
+  const validPostcode = (postcode) => {
+    postcode = postcode.replace(/\s/g, "").toUpperCase();
+    const regex = /^[A-Z]{1,2}[0-9]{1,2}[A-Z]{0,1} ?[0-9][A-Z]{2}$/i
+    return regex.test(postcode);
+  }
+
   return (
     <div className="mb-10">
       <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
@@ -21,35 +88,46 @@ export const UserRegistrationForm = () => {
       <p className="mt-2 text-center text-sm text-gray-600 mt-5">
         Complete the form to sign up {' '}
       </p>
-      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
-            Name
+      { duplicateEmail &&
+      <h2 className="mt-6 text-center text-3xl font-extrabold text-orange-400">
+        This email has already registered
+      </h2>
+      }
+      { userCreated &&
+      <h2 className="mt-6 text-center text-3xl font-extrabold text-green-400">
+        Thanks for registering!
+      </h2>
+      }
+      <form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="name">
+            Name (optional)
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="name" onChange={() => setName()} />
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="name" value={name} onChange={(e) => setName(e.target.value)} />
         </div>
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="name">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
             Email
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="text" placeholder="email" />
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          {emailInvalid && <p className="text-red-500 text-xs italic">Please enter a valid email.</p>}
         </div>
-        <div class="mb-6">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="password">
             Password
           </label>
-          
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="text" placeholder="************" />
-          <p class="text-red-500 text-xs italic">Please choose a password.</p>
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="************" value={password} onChange={(e) => setPassword(e.target.value)} />
+          {passwordInvalid && <p className="text-red-500 text-xs italic">Please choose a password.</p>}
         </div>
-        <div class="mb-6">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
-            Postcode
+        <div className="mb-6">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="postcode">
+            Postcode (optional)
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="postcode" type="text" placeholder="postcode" />
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="postcode" type="text" placeholder="postcode" value={postcode} onChange={(e) => setPostcode(e.target.value)} />
+          {postcodeInvalid && <p className="text-orange-500 text-xs italic">The postcode you have entered is invalid.</p>}
         </div>
-        <div class="flex items-center justify-between">
-          <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+        <div className="flex items-center justify-between">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button" onClick={saveUser}>
             Register
           </button>
         </div>
