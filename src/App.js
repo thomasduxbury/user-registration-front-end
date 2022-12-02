f993b67ea180c7e80d063b2bac5119713f472d01
Thomas Duxbury <thomas.duxbury90@gmail.com>
2022-12-02 10:55:27 +0000

    Formatting App.js

--- src/App.js
+++ src/App.js
@@ -4,11 +4,10 @@ import { UserRegistrationForm } from './UserRegistrationForm';
 function App() {
   return (
     <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div className="max-w-md w-full space-y-8">
-
-    <UserRegistrationForm/>
-        </div>
-        </div>
+      <div className="max-w-md w-full space-y-8">
+        <UserRegistrationForm />
+      </div>
+    </div>
   );
 }
 
